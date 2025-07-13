e54c4db1aee648e960148fff42c2b0110814fd29
Muhammad Abdurrofi <muhammad.abdurrofi@bps.go.id>
2025-07-13 14:32:23 +0800

    Perbaikan agar website mengarah ke https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if($this->app->environment('production') || $this->app->environment('staging')) {
+            URL::forceScheme('https');
+            request()->server->set('HTTPS', request()->header('X-Forwarded-Proto', 'https') == 'https' ? 'on' : 'off');
+        }
     }
 }
